244124f0c05dd7667af158171c3b943a8dd68c18
Breno Almeida <kbreno08@gmail.com>
2020-08-07 13:43:23 -0300

    Verificando o deploy

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
     ?
     'http://localhost:8080'
     :
-    'http://brenoflix.herokuapp.com'
+    'https://brenoflix.herokuapp.com'
     ;
 
 export default {
